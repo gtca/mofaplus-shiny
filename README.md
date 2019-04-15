9916656da728cf09f24f8b01b489a7db1d21fe4e
Danila Bredikhin <danila.bredikhin@embl.de>
2019-04-15 02:03:59 +0200

    Implement plotting loadings and factor values
    Closes #3

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 MOFA+ models made interactive with Shiny by RStudio.
 
 See [more on MOFA+ here](https://github.com/biofam/BioFAM/).
+
+See [more on Shiny app development here](http://shiny.rstudio.com/tutorial/).
--- app/app.R
+++ app/app.R
@@ -8,40 +8,117 @@
 #
 
 library(shiny)
+library(shinyWidgets)
+
+library(devtools)
+load_all('../../BioFAM/BioFAMtools/')
+
+options(shiny.maxRequestSize = 300*1024^2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("MOFA+ model exploration"),
+    
 
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    # sidebarLayout(
+        # sidebarPanel(
+        #     # sliderInput("bins",
+        #     #             "Number of bins:",
+        #     #             min = 1,
+        #     #             max = 50,
+        #     #             value = 30),
+        #     # HDF5 model file picker
+        #     fileInput(inputId = "model_hdf_file",
+        #               label = "Model HDF5 file:",
+        #               buttonLabel = "Model file",
+        #               accept = "hdf5")
+        #                 
+        # ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            tabsetPanel(
+                tabPanel("Model overview", 
+                    # HDF5 model file picker
+                    fileInput(inputId = "model_hdf_file",
+                              label = "Model HDF5 file:",
+                              buttonLabel = "Model file",
+                              accept = "hdf5"),
+                    plotOutput("modelOverviewPlot")
+                ),
+                tabPanel("Loadings", 
+                         uiOutput("factorsChoice_loadings"),
+                         # sidebarPanel(selectInput("factorsChoice", "Choose one or more factors:", choices = 1:2, multiple = TRUE)),
+                         plotOutput("weightsPlot")
+                ),
+                tabPanel("Factors", 
+                         uiOutput("factorsChoice_factors"),
+                         plotOutput("factorsPlot")
+                )
+            )
         )
-    )
+    # )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    model <- reactive({
+        input_model <- input$model_hdf_file
+        if (is.null(input_model)) return(NULL)
+        load_model(input_model$datapath)
+    })
+    
+    factorsChoice <- reactive({
+        m <- model()
+        if (is.null(m)) return(NULL)
+        factors_names(m)
+    })
+    
+    factorsSelection_loadings <- reactive({
+        if (is.null(input$factorsChoice_loadings))
+            return("all")
+        input$factorsChoice_loadings
+    })
+    
+    factorsSelection_factors <- reactive({
+        if (is.null(input$factorsChoice_factors))
+            return("all")
+        input$factorsChoice_factors
+    })
+    
+    # userSelection <- reactiveValues(factorsSelection = reactive({
+    #     if (is.null(input$factorsChoice))
+    #         return("all")
+    #     input$factorsChoice
+    # }))
+    
+    output$factorsChoice_loadings <- renderUI({
+        selectInput('factorsChoice_loadings', 'Factors:', choices = factorsChoice(), multiple = TRUE, selected = factorsSelection_factors())
+    })
+    output$factorsChoice_factors <- renderUI({
+        selectInput('factorsChoice_factors', 'Factors:', choices = factorsChoice(), multiple = TRUE, selected = factorsSelection_loadings())
+    })
+    
+    output$modelOverviewPlot <- renderPlot({
+        m <- model()
+        if (is.null(m)) return(NULL)
+        plot_data_overview(m)
+    })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$weightsPlot <- renderPlot({
+        m <- model()
+        if (is.null(m)) return(NULL)
+        plot_weights(m, factors = factorsSelection_loadings()) 
+    })
+    
+    output$factorsPlot <- renderPlot({
+        m <- model()
+        if (is.null(m)) return(NULL)
+        plot_factors_jitter(m, factors = factorsSelection_factors()) 
     })
 }
 
