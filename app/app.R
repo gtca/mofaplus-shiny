5d118dd82deaa83977fee26efb42613bc8cba45c
Danila Bredikhin <danila.bredikhin@embl.de>
2019-08-30 18:40:51 +0200

    Improve factors plot and others
    - When plotting loadings, the number of features to label can be
    adjusted
    - X axis for factors plot can be chosen as well as groups to be
    displayed and representation (dots, violins)
    - Model overview was renamed to Data overview
    - Misc small fixes

--- app/app.R
+++ app/app.R
@@ -11,7 +11,8 @@ library(shiny)
 library(shinyWidgets)
 
 library(devtools)
-load_all('../../BioFAM/BioFAMtools/')
+#library(MOFA2)
+load_all('../../MOFA2/MOFA2')
 
 options(shiny.maxRequestSize = 1000*1024^2)
 
@@ -39,8 +40,8 @@ ui <- fluidPage(theme = "styles.css",
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel("Model overview", 
-                    plotOutput("modelOverviewPlot")
+                tabPanel("Data overview", 
+                    plotOutput("dataOverviewPlot")
                 ),
                 tabPanel("Variance", 
                          # TODO: selector x, y
@@ -49,19 +50,32 @@ ui <- fluidPage(theme = "styles.css",
                          plotOutput("varianceExplainedPlot")
                 ),
                 tabPanel("Loadings", 
-                         # TODO: show/hide labels of N genes
-                         # TODO: N ^
-                         # TODO: Sort values by factor ...
+                         fluidRow(
+                            column(5, sliderInput(inputId = "nfeatures_to_label",
+                                                  label = "Number of features to label",
+                                                  min = 0,
+                                                  max = 100,
+                                                  value = 10,
+                                                  step = 1))
+                         ),
                          plotOutput("weightsPlot")
+                         # plotOutput("weightsPlot", hover = "weightsHover")
+                         # verbatimTextOutput("weightsInfo")
                 ),
                 tabPanel("Factors", 
-                         # TODO: group_by
+                         fluidRow(
+                            column(2, uiOutput("factorsAxisChoice_x")),
+                            column(3, uiOutput("factorsGroupsChoice")),
+                            column(2, materialSwitch(inputId = "factorsAddDots", label = "Add dots", status = "default", value = TRUE)),
+                            column(2, materialSwitch(inputId = "factorsAddViolins", label = "Add violins", status = "default", value = FALSE))
+                         ),
+                         hr(),
                          plotOutput("factorsPlot")
                 ),
                 tabPanel("Embeddings", 
                          fluidRow(
                             column(3, uiOutput("factorChoice_x")),
-                            column(1, actionButton("swapEmbeddings", "swap", 
+                            column(1, actionButton("swapEmbeddings", "", 
                                                    icon("exchange-alt"),
                                                    style = "margin: 25px 0;")),
                             column(3, uiOutput("factorChoice_y"))
@@ -79,6 +93,11 @@ ui <- fluidPage(theme = "styles.css",
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
+    ###########################
+    ### REACTIVE COMPONENTS ###
+
+    ### GLOBAL VARIABLES ###
     
     model <- reactive({
         input_model <- input$model_hdf_file
@@ -92,13 +111,13 @@ server <- function(input, output) {
         factors_names(m)
     })
     
-    coloursChoice <- reactive({
+    metaChoice <- reactive({
         m <- model()
         if (is.null(m)) return(NULL)
         metadata_names <- colnames(samples_metadata(m))
         metadata_names[metadata_names != "sample_name"]
     })
-    
+
     factorsSelection <- reactive({
         if (is.null(input$factorsChoice))
             return("all")
@@ -111,7 +130,7 @@ server <- function(input, output) {
         input$colourChoice
     })
     
-    
+    ### EMBERDDINGS ###
     
     factorSelection_x <- reactive({
         if (is.null(input$factorChoice_x)) {
@@ -140,6 +159,35 @@ server <- function(input, output) {
         }
         input$factorChoice_y
     })
+
+    ### FACTOR VALUES ###
+
+    factorsAxisSelection_x <- reactive({
+        if (is.null(input$colourChoice)) {
+            selected_global <- input$colourChoice
+            if (is.null(selected_global)) {
+                return(metaChoice()[1])
+            } else if (length(selected_global) >= 1) {
+                return(selected_global[1])
+            } else {
+                return(metaChoice()[1])
+            }
+        }
+        input$colourChoice
+    })
+
+
+    factorsGroupsChoice <- reactive({
+        m <- model()
+        if (is.null(m)) return(NULL)
+        as.character(unique(samples_metadata(m)[,factorsAxisSelection_x()]))
+    })
+    
+
+
+    #################
+    ### RENDERING ###
+    #################
     
     
     output$factorsChoice <- renderUI({
@@ -147,14 +195,18 @@ server <- function(input, output) {
     })
     
     output$colourChoice <- renderUI({
-        selectInput('colourChoice', 'Colour cells:', choices = coloursChoice(), multiple = FALSE, selectize = TRUE)
+        selectInput('colourChoice', 'Colour cells:', choices = metaChoice(), multiple = FALSE, selectize = TRUE)
     })
+
+    ### MODEL OVERVIEW ###
     
-    output$modelOverviewPlot <- renderPlot({
+    output$dataOverviewPlot <- renderPlot({
         m <- model()
         if (is.null(m)) return(NULL)
         plot_data_overview(m)
     })
+
+    ### VARIANCE EXPLAINED ###
     
     output$varianceExplainedPlot <- renderPlot({
         m <- model()
@@ -164,28 +216,55 @@ server <- function(input, output) {
                                 plot_total = FALSE)
     })
 
+    ### WEIGHTS (LOADINGS) ###
+
     output$weightsPlot <- renderPlot({
         m <- model()
         if (is.null(m)) return(NULL)
-        plot_weights(m, factors = factorsSelection()) 
+        plot_weights(m, factors = factorsSelection(), nfeatures = input$nfeatures_to_label)
     })
+
+    # output$weightsInfo <- renderPrint({
+    #     m <- model()
+    #     if (is.null(m)) return(NULL)
+    #     nearPoints(plot_weights(m, factors = factorsSelection(), nfeatures = input$nfeatures_to_label, return_data = TRUE),
+    #                input$weightsHover,
+    #                threshold = 10, maxpoints = 3, addDist = TRUE)
+    # })
+
+    ### FACTOR VALUES ###
     
     output$factorsPlot <- renderPlot({
         m <- model()
         if (is.null(m)) return(NULL)
-        plot_factors(m, factors = factorsSelection(), color_by = colourSelection()) 
+        plot_factor(m, factors = factorsSelection(), color_by = colourSelection(), group_by = factorsAxisSelection_x(), 
+                    groups = input$factorsGroupsChoice, add_dots = input$factorsAddDots, add_violin = input$factorsAddViolins)
     })
+
+    output$factorsAxisChoice_x <- renderUI({
+        selectInput('factorsAxisChoice_x', 'X axis:',
+                    choices = metaChoice(), multiple = FALSE, selectize = TRUE,
+                    selected = factorsAxisSelection_x())
+    })
+
+    output$factorsGroupsChoice <- renderUI({
+        selectInput('factorsGroupsChoice', 'Display groups:',
+                    choices = factorsGroupsChoice(), multiple = TRUE, selectize = TRUE,
+                    selected = factorsGroupsChoice())
+    })
+
+    ### FACTORS SCATTERPLOT (EMBEDDINGS) ###
     
     output$embeddingsPlot <- renderPlot({
         m <- model()
         if (is.null(m)) return(NULL)
-        plot_embeddings(m, factors = c(factorSelection_x(), factorSelection_y()), color_by = colourSelection()) 
+        plot_factors(m, factors = c(factorSelection_x(), factorSelection_y()), color_by = colourSelection()) 
     })
     
     output$embeddingsInfo <- renderPrint({
         m <- model()
         if (is.null(m)) return(NULL)
-        df <- plot_embeddings(m, factors = c(factorSelection_x(), factorSelection_y()), color_by = colourSelection(), return_data = TRUE) 
+        df <- plot_factors(m, factors = c(factorSelection_x(), factorSelection_y()), color_by = colourSelection(), return_data = TRUE) 
         brushedPoints(df, input$plot_brush)
     })
     
