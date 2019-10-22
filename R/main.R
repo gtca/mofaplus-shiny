d2039bb8c165095629e47b50822bf42d505324e9
Danila Bredikhin <danila.bredikhin@embl.de>
2019-10-22 16:26:16 +0200

    Rename launch function to shiny_mofa()

--- R/main.R
+++ R/main.R
@@ -1,7 +1,7 @@
 #' Function to run Shiny from R
 #' @import shiny
 #' @examples
-#' shiny_mofa2(port = 8400)
-shiny_mofa2 <- function(...) {
+#' shiny_mofa(port = 8400)
+shiny_mofa <- function(...) {
     shiny::runApp(system.file('app', package = "mofaplusshiny"), ...)
 }
